5c8f237b87fb808274989a3a04ea92b84d507fa5
laptop <yoseyus@gmail.com>
2026-07-07 10:34:02 +0200

    operators: add rb and rr

--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: jzaquina <jzaquina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 18:38:09 by jzaquina          #+#    #+#             */
-/*   Updated: 2026/07/02 20:32:47 by jzaquina         ###   ########.fr       */
+/*   Updated: 2026/07/07 10:33:23 by jzaquina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_cpystack(t_stack *src, t_stack *dst)
 	dst->value = src->value;
 }
 
-void	ra(t_stack *stack)
+void	rotate(t_stack *stack)
 {
 	t_stack	*stk_first;
 
@@ -39,5 +39,23 @@ void	ra(t_stack *stack)
 		stack = stack->next;
 	}
 	free(stk_first);
+}
+
+void	ra(t_stack *stack_a)
+{
+	rotate(stack_a);
 	write(1, "ra\n", 3);
+}
+
+void	rb(t_stack *stack_b)
+{
+	rotate(stack_b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_stack *stack_a, t_stack *stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	write(1, "rr\n", 3);
 }
